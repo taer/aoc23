54148a8f38edc4f5fa1a34531aecc4d184e7b01c
Robert <robert.macaulay@gmail.com>
2023-12-03 17:16:39 -0600

    tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,15 @@ repositories {
     mavenCentral()
 }
 
+dependencies{
+    val kotestVersion = "5.8.0"
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
 tasks {
     wrapper {
         gradleVersion = "8.4"
