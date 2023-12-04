ce9afe0994867a133934144fd642dd99872a8fbd
Robert <robert.macaulay@gmail.com>
2023-12-04 15:28:40 -0600

    day04

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -2,46 +2,55 @@ package day04
 
 import println
 import readInput
+import kotlin.math.pow
 
 fun main() {
-    fun double(wins: Int): Int {
-        if(wins==0) return 0
-        var nummer = 1
-        repeat(wins-1){
-            nummer *=2
-        }
-        return nummer //use a fold. just dont wanna
-    }
+    fun double(wins: Int) = 2.0.pow(wins-1).toInt()
+
     val delimiters = " +".toRegex()
+
+    fun winsFromInput(input: List<String>) = input.map {
+        val (winners, ours) = it.split(":", limit = 2)[1]
+            .split("|", limit = 2)
+            .map {
+                it.trim()
+                    .split(delimiters)
+                    .map { it.toInt() }
+            }
+        ours.count { ourNumber -> winners.contains(ourNumber) }
+    }
+
     fun part1(input: List<String>): Int {
-        val wins = input.map {
-            val (winners, ours) = it.split(":", limit = 2)[1]
-                .split("|", limit = 2)
-                .map {
-                    it.trim()
-                        .split(delimiters)
-                        .map { it.toInt() }
-                }
-            ours.count { winners.contains(it) }
-        }
+        val wins = winsFromInput(input)
         return wins.sumOf {
             double(it)
         }
     }
 
-    fun part2(input: List<String>): Long {
-return 1L
+    fun part2(input: List<String>): Int {
+        val wins = winsFromInput(input)
+
+        val cardCount = MutableList(input.size){ 1 }
+        wins.forEachIndexed{index, winsForCard ->
+            repeat(cardCount[index]) {
+                repeat(winsForCard) { cardToAddTo ->
+                    val toMod = index + cardToAddTo + 1
+                    if (toMod < wins.size) {
+                        cardCount[toMod] += 1
+                    }
+                }
+            }
+        }
+        return cardCount.sum()
     }
 
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 13)
-//    check(part2(testInput) == 467835L)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     part1(input).println()
     part2(input).println()
-//    check(part1(input) == 528799)
-//    check(part2(input) == 84907174L)
+    check(part1(input) == 25174)
+    check(part2(input) == 6420979)
 }
