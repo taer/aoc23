a4d33f240d3e304ab50c52874383397b81fd72c8
Robert <robert.macaulay@gmail.com>
2023-12-12 10:21:19 -0600

    day11

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -1,26 +1,76 @@
 package day11
 
+import point.Point
 import println
 import readInput
+import kotlin.math.absoluteValue
 
 fun main() {
-
-    fun part1(input: List<String>): Int {
-        return 1
+    fun makePairs(arr: List<Point>): Sequence<Pair<Point, Point>> = sequence {
+        for(i in 0 until arr.size-1) {
+            for(j in i+1 until arr.size) {
+                yield(arr[i] to arr[j])
+            }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return 1
+    fun distanceFrom(from: Point, to: Point) =
+        (from.x - to.x).absoluteValue.toLong() + (from.y - to.y).absoluteValue.toLong()
+
+    fun makeColumns(input: List<String>) =
+        input.first().indices.map { colIdx -> input.map { row -> row[colIdx] } }
+
+    fun holeyStars(input: List<String>, multiplier: Int): Long {
+        val rowsShift = input.map {
+            if (it.any { it == '#' }) 0 else 1
+        }.toMutableList()
+        for (i in 1 until rowsShift.size) {
+            rowsShift[i] = rowsShift[i] + rowsShift[i - 1]
+        }
+
+        val map = makeColumns(input)
+        val colShift = map.map {
+            if (it.any { it == '#' }) 0 else 1
+        }.toMutableList()
+        for (i in 1 until colShift.size) {
+            colShift[i] = colShift[i] + colShift[i - 1]
+        }
+
+        val points = input.flatMapIndexed { x, row ->
+            row.mapIndexedNotNull { y, c ->
+                if (c == '#') {
+                    Point(
+                        x = x + rowsShift[x] * (multiplier-1),
+                        y = y + colShift[y] * (multiplier-1)
+                    )
+                } else {
+                    null
+                }
+            }
+        }
+        return makePairs(points).sumOf { (from, to) ->
+            distanceFrom(from, to)
+        }
+    }
+
+    fun part1(input: List<String>): Long {
+        return holeyStars(input, 2)
+    }
+
+    fun part2(input: List<String>): Long {
+        return holeyStars(input, 1_000_000)
     }
 
     val testInput = readInput("Day11_test")
-    check(part1(testInput) == 4)
-//    check(part2(testInput) == 4)
+    part1(testInput).println()
+    check(part1(testInput) == 374L)
+    check(holeyStars(testInput,10) == 1030L)
+    check(holeyStars(testInput,100) == 8410L)
 
     val input = readInput("Day11")
     part1(input).println()
     part2(input).println()
-//    check(part1(input) == 6864)
-//    check(part2(input) == 349)
+    check(part1(input) == 9522407L)
+    check(part2(input) == 544723432977L)
 }
 
