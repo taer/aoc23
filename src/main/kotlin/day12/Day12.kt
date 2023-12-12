e75b5ec0ae6e9c5cc80bc0d885cfb9a7634adbea
Robert <robert.macaulay@gmail.com>
2023-12-12 16:55:01 -0600

    pre-cache

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -1,21 +1,27 @@
 package day12
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import println
 import readInput
 
 fun main() {
 
 
-    fun permute(it: String): List<String>{
-        if(!it.contains("?")) return listOf(it)
-
-        val replaceFirst = it.replaceFirst("?", ".")
-        val replaceFirst2 = it.replaceFirst("?", "#")
-        return listOf(permute(replaceFirst), permute(replaceFirst2)).flatten()
+    fun permute2(it: String): Flow<String> = flow<String>{
+        if(!it.contains("?")) {
+            emit(it)
+        }else{
+            emitAll(permute2(it.replaceFirst("?", ".")))
+            emitAll(permute2(it.replaceFirst("?", "#")))
+        }
     }
-    
+
     val delimiters = "\\.+".toRegex()
-    fun matchesChecksun(springs: String, buckets: List<Int>): Boolean {
+    fun matchesBucket(springs: String, buckets: List<Int>): Boolean {
         val split = springs.split(delimiters)
             .filter { it.isNotEmpty() }
             .map { it.length }
@@ -26,37 +32,94 @@ fun main() {
         val (springs, bucketStr ) = line.split(" ", limit = 2)
         val buckets = bucketStr.split(",").map { it.toInt() }
 
-        val permute = permute(springs)
+        val permute = permute2(springs)
 
-        return permute.count {
-            matchesChecksun(it, buckets)
+        return runBlocking {
+            permute.count {
+                matchesBucket(it, buckets)
+            }
         }
 
     }
 
-    fun part1(input: List<String>): Int {
+    val cache = mutableMapOf<Pair<String,List<Int>>, Long>()
+    fun count(springs: String, buckets: List<Int>): Long {
+        val cached = cache.get(springs to buckets)
+        if(cached !=null) return cached
+
+        val emptySpring = springs.trim('.').isEmpty()
+        if(emptySpring){
+            return if(buckets.isEmpty()) 1 else 0
+        }
+
+        val droppedFirstLetter = springs.drop(1)
+        val firstLetter = springs[0]
+        if(firstLetter == '.'){
+            return count(droppedFirstLetter, buckets)
+        }
+        if(firstLetter == '?'){
+            val dotFirst = count(".$droppedFirstLetter", buckets)
+            val hashFirst = count("#$droppedFirstLetter", buckets)
+            return dotFirst + hashFirst
+        }
+        if(firstLetter == '#'){
+            if(buckets.isEmpty())return 0
+            val amount = buckets.first()
+            if(amount > springs.length)
+                return 0
+            val hashes = springs.take(amount).all{ it == '#' || it == '?' }
+            if(hashes){
+                val nextBuckets = buckets.drop(1)
+                if(springs.length==amount){
+                    return if(nextBuckets.isEmpty()) 1 else 0
+                }
+                val nextAfterPairing = springs.getOrNull(amount) //next char
+                if(nextAfterPairing==null && nextBuckets.isEmpty()){
+                    return 1
+                }
+                return if(nextAfterPairing=='#'){
+                    0
+                }else{
+                    count(springs.drop(amount+1), nextBuckets)
+                }
+
+            }
+        }
+        return 0
+
+    }
+
+    fun doWork2(line: String): Long {
+        val (springs, bucketStr ) = line.split(" ", limit = 2)
+        val buckets = bucketStr.split(",").map { it.toInt() }
+
+        val count = count(springs.trim(('.')), buckets)
+        return count
+
+    }
+
+    fun part1(input: List<String>): Long {
         return input.sumOf {
-            doWork(it)
+            doWork2(it)
         }
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         return input.sumOf {
             val (a,b) = it.split(" ")
-            val expando = "$a?$a?$a?$a?$a $b,$b,$b,$b,$b"
-            doWork(expando)
+            val expanded = List(5){a}.joinToString("?") + " " + List(5){b}.joinToString(",")
+            doWork2(expanded)
         }
     }
 
     val testInput = readInput("Day12_test")
-    part1(testInput).println()
-    check(part1(testInput) == 21)
-    check(part2(testInput) == 525152)
+    check(part1(testInput) == 21L)
+    check(part2(testInput) == 525152L)
 
     val input = readInput("Day12")
     part1(input).println()
     part2(input).println()
-    check(part1(input) == 7169)
+    check(part1(input) == 7169L)
 //    check(part2(input) == 544723432977L)
 }
 
