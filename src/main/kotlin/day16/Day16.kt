ae642c76354b4e7003954c846b3b05a1aa6d2aa1
Robert <robert.macaulay@gmail.com>
2023-12-16 01:21:41 -0600

    da16: made BFS and part2

--- src/main/kotlin/day16/Day16.kt
+++ src/main/kotlin/day16/Day16.kt
@@ -5,111 +5,129 @@ import point.Point
 import point.move
 import println
 import readInput
-import java.lang.RuntimeException
+import java.util.*
 
 fun main() {
 
-    data class MovingState(val point: Point, val movingTowards: Direction)
+    data class State(val point: Point, val movingTowards: Direction)
 
-    fun MovingState.moveNext() = point.move(movingTowards)
+    fun State.makeMove(dir: Direction? = null) = (dir ?: this.movingTowards).let {
+        copy(
+            movingTowards = it,
+            point = point.move(it)
+        )
+    }
 
     fun List<List<Char>>.inMap(point: Point) =
         (point.x >= 0 && point.y >= 0 && point.x < size && point.y < first().size)
 
-    class WalingContext(val map: List<List<Char>>){
+
+    fun doMoves(start: State, map: List<List<Char>>): Array<Array<Int>> {
+        val seenNodes = mutableSetOf<State>()
         val colors = Array(map.size) {
             Array(map.first().size) { 0 }
         }
-        val seenNodes = mutableSetOf<MovingState>()
-    }
+        val queue = LinkedList<State>().also { it.add(start) }
 
-    fun doMoves(movement: MovingState, state: WalingContext) {
-        
+        while (queue.isNotEmpty()) {
+            val currentState = queue.pop()
 
-
-
-
-
-
-        if (!state.map.inMap(movement.point)) {
-            return
-        }
-
-        if(state.seenNodes.contains(movement)){
-            return
-        }
-        state.seenNodes.add(movement)
-        state.colors[movement.point.x][movement.point.y]++
-
-        when (state.map[movement.point.x][movement.point.y]) {
-            '.' -> {
-                doMoves(movement.copy(point = movement.moveNext()), state)
+            if (!map.inMap(currentState.point)) {
+                continue
             }
 
-            '/' -> {
-                val newDir = when (movement.movingTowards) {
-                    Direction.N -> Direction.E
-                    Direction.S -> Direction.W
-                    Direction.E -> Direction.N
-                    Direction.W -> Direction.S
+            if (seenNodes.contains(currentState)) {
+                continue
+            }
+            seenNodes.add(currentState)
+            colors[currentState.point.x][currentState.point.y]++
+
+            when (map[currentState.point.x][currentState.point.y]) {
+                '.' -> {
+                    queue.add(currentState.makeMove())
                 }
-                doMoves(movement.copy(point = movement.point.move(newDir), movingTowards = newDir), state)
-            }
 
-            '\\' -> {
-                val newDir = when (movement.movingTowards) {
-                    Direction.N -> Direction.W
-                    Direction.S -> Direction.E
-                    Direction.E -> Direction.S
-                    Direction.W -> Direction.N
+                '/' -> {
+                    val newDir = when (currentState.movingTowards) {
+                        Direction.N -> Direction.E
+                        Direction.S -> Direction.W
+                        Direction.E -> Direction.N
+                        Direction.W -> Direction.S
+                    }
+                    queue.add(currentState.makeMove(newDir))
                 }
-                doMoves(movement.copy(point = movement.point.move(newDir), movingTowards = newDir), state)
-            }
 
-            '|' -> {
-                when (movement.movingTowards) {
-                    Direction.N, Direction.S ->
-                        doMoves(movement.copy(point = movement.moveNext()), state)
+                '\\' -> {
+                    val newDir = when (currentState.movingTowards) {
+                        Direction.N -> Direction.W
+                        Direction.S -> Direction.E
+                        Direction.E -> Direction.S
+                        Direction.W -> Direction.N
+                    }
+                    queue.add(currentState.makeMove(newDir))
+                }
 
-                    Direction.E, Direction.W -> {
-                        doMoves(movement.copy(point = movement.point.move(Direction.N), movingTowards = Direction.N), state)
-                        doMoves(movement.copy(point = movement.point.move(Direction.S), movingTowards = Direction.S), state)
+                '|' -> {
+                    when (currentState.movingTowards) {
+                        Direction.N, Direction.S -> queue.add(currentState.makeMove())
+
+                        Direction.E, Direction.W -> {
+                            queue.add(currentState.makeMove(Direction.N))
+                            queue.add(currentState.makeMove(Direction.S))
+                        }
                     }
                 }
-            }
 
-            '-' -> {
-                when (movement.movingTowards) {
-                    Direction.E, Direction.W ->
-                        doMoves(movement.copy(point = movement.moveNext()), state)
+                '-' -> {
+                    when (currentState.movingTowards) {
+                        Direction.E, Direction.W -> queue.add(currentState.makeMove())
 
-                    Direction.N, Direction.S -> {
-                        doMoves(movement.copy(point = movement.point.move(Direction.E), movingTowards = Direction.E), state)
-                        doMoves(movement.copy(point = movement.point.move(Direction.W), movingTowards = Direction.W), state)
+                        Direction.N, Direction.S -> {
+                            queue.add(currentState.makeMove(Direction.E))
+                            queue.add(currentState.makeMove(Direction.W))
+                        }
                     }
                 }
-            }
 
-            else -> throw RuntimeException("foo ")
+                else -> throw RuntimeException("foo ")
+            }
         }
+        return colors
     }
 
     fun part1(input: List<String>): Int {
         val map = input.map { it.toList() }
-        val start = MovingState(Point(0, 0), Direction.E)
-        val state = WalingContext(map)
-        doMoves(start, state)
-        println()
-        state.colors.forEach {
-            println( it.joinToString("" ){  if(it==0) "." else "#"}   )
-        }
-        println()
-        return state.colors.sumOf { pp -> pp.count { aa -> aa != 0 } }
+        val start = State(Point(0, 0), Direction.E)
+        val colors = doMoves(start, map)
+//        println()
+//        state.colors.forEach {
+//            println(it.joinToString("") { if (it == 0) "." else "#" })
+//        }
+//        println()
+        return colors.sumOf { pp -> pp.count { aa -> aa != 0 } }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val map = input.map { it.toList() }
+        val startNS = (map.first().indices).flatMap {
+            listOf(
+                State(Point(0, it), Direction.S),
+                State(Point(map.size - 1, it), Direction.N),
+            )
+        }
 
+        val startEW = map.indices.flatMap {
+            listOf(
+                State(Point(it, 0), Direction.E),
+                State(Point(it, map.first().size - 1), Direction.W),
+            )
+        }
+        val total = startEW + startNS
+
+        return total.maxOf {
+            val colors = doMoves(it, map)
+            colors.sumOf { pp -> pp.count { aa -> aa != 0 } }
+        }
     }
 
     val testInput = readInput("Day16_test")
@@ -117,12 +135,12 @@ fun main() {
     check(part1 == 46) {
         part1
     }
-//    check(part2(testInput) == 145)
+    check(part2(testInput) == 51)
 
     val input = readInput("Day16")
     part1(input).println()
     part2(input).println()
-//    check(part1(input) == 494980)
-//    check(part2(input) == 247933)
+    check(part1(input) == 8901)
+    check(part2(input) == 9064)
 }
 
